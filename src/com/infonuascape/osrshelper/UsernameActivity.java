4c982f834590a71529d7d15d13292e230ec20d6f
Marc-Antoine Hinse <marc_antoine_hinse@hotmail.com>
2015-09-06 21:34:59 -0400

    New button delete usernames lvls needed til next combat lvl

--- src/com/infonuascape/osrshelper/UsernameActivity.java
+++ src/com/infonuascape/osrshelper/UsernameActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
