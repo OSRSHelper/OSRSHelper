a33b77c909f4fd052670541695729da247b13930
Louis Dion-Marcil <louisdionmarcil@gmail.com>
2017-07-09 15:35:47 -0400

    Use APIError over ParseError to display proper errors

--- app/src/main/java/com/infonuascape/osrshelper/tracker/rt/TrackerHelper.java
+++ app/src/main/java/com/infonuascape/osrshelper/tracker/rt/TrackerHelper.java
@@ -1,6 +1,7 @@
 package com.infonuascape.osrshelper.tracker.rt;
 
 import com.infonuascape.osrshelper.tracker.TrackerTimeEnum;
+import com.infonuascape.osrshelper.utils.exceptions.APIError;
 import com.infonuascape.osrshelper.utils.exceptions.ParserErrorException;
 import com.infonuascape.osrshelper.utils.exceptions.PlayerNotFoundException;
 import com.infonuascape.osrshelper.utils.players.PlayerSkills;
@@ -16,13 +17,13 @@ public class TrackerHelper {
 		return userName;
 	}
 
-	public PlayerSkills getPlayerStats() throws PlayerNotFoundException, ParserErrorException {
+	public PlayerSkills getPlayerStats() throws PlayerNotFoundException, ParserErrorException, APIError {
 		TrackerFetcher tf = new TrackerFetcher(getUserName(), TrackerTimeEnum.TrackerTime.Day);
 		return tf.getPlayerTracker();
 	}
 
 	public PlayerSkills getPlayerStats(TrackerTimeEnum.TrackerTime time) throws PlayerNotFoundException,
-			ParserErrorException {
+			ParserErrorException, APIError {
 		TrackerFetcher tf = new TrackerFetcher(getUserName(), time);
 		return tf.getPlayerTracker();
 	}
